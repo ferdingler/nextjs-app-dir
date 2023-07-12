a213b05c6b1c6936452cafd680b57ddbf3db23eb
Fernando Dingler <fdingler@amazon.com>
2023-07-12 14:35:15 -0700

    from public folder data

--- src/app/api/hello.ts
+++ src/app/api/hello.ts
@@ -1,15 +1,18 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import dataFromPublicFolder from "../../public/data.json";
-import dataFromApiFolder from "./data/data.json";
+import fromPublicFolder from "../../../public/data.json";
+import fromApiFolder from "./data/data.json";
 
-type Data = string;
+type Data = {
+  fromPublicFolder: any;
+  fromApiFolder: any;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
   res.status(200).json({
-    fromPublicFolder: JSON.stringify(dataFromPublicFolder),
-    dataFromApiFolder: JSON.stringify(dataFromApiFolder),
+    fromPublicFolder,
+    fromApiFolder,
   })
 }
