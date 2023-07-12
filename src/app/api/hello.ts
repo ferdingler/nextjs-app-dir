1391c22575e7c25850faa2dd1bd18dc53b25ea72
Fernando Dingler <fdingler@amazon.com>
2023-07-12 14:50:31 -0700

    using fs

--- src/app/api/hello.ts
+++ src/app/api/hello.ts
@@ -1,9 +1,15 @@
 import fromPublicFolder from "../../../public/data.json";
 import fromApiFolder from "./data/data.json";
+import fs from "fs";
+
+const usingFsFromPublicFolder = fs.readFileSync("../../../public/data.json", "utf-8");
+const usingFsFromApiRoute = fs.readFileSync("./data/data/json", "utf-8");
 
 export default function handler() {
   return {
     fromPublicFolder,
     fromApiFolder,
+    usingFsFromPublicFolder,
+    usingFsFromApiRoute,
   };
 }
