c57fe2865ca2ede7e6571e3af869c7bfbcafe293
Fernando Dingler <fdingler@amazon.com>
2023-07-12 15:30:28 -0700

    PDF

--- src/app/api/hello.ts
+++ src/app/api/hello.ts
@@ -4,15 +4,17 @@ import path from 'path';
 import { promises as fs } from 'fs';
 
 export default async function handler() {
-  //Find the absolute path of the json directory
+  // Find the absolute path of the json directory
   const jsonDirectory = path.join(process.cwd(), 'json');
 
-  //Read the json data file data.json
+  // Read the json data file data.json
   const fileContents = await fs.readFile(jsonDirectory + '/data.json', 'utf8');
+  const pdfContents = await fs.readFile(jsonDirectory + '/zero2prod.pdf', 'utf8');
 
   return {
     fromPublicFolder,
     fromApiFolder,
     usingFs: fileContents,
+    pdfContents,
   };
 }
